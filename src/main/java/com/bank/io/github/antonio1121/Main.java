ad1a898abb1ee2342b4c7028652f897900f84690
Antonio <antoniohalkiadakis@gmail.com>
2025-04-30 01:38:12 -0400

    changed package name

--- src/main/java/com/bank/io/github/antonio1121/Main.java
+++ src/main/java/com/bank/io/github/antonio1121/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.bank.io.github.antonio1121;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -7,7 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
-
+        //TODO refactor package name with github pages website
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
